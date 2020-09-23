3ba4dfe746daed7235dbd2e992c7e2bfb518cfe9
Domenic Catalano <domenic.catalano@dcmail.ca>
2020-09-23 11:40:19 -0400

    added operator >> and operator << to CartesianPoint

--- OOP3200-F2020-Lesson2/CartesianPoint.h
+++ OOP3200-F2020-Lesson2/CartesianPoint.h
@@ -1,3 +1,8 @@
+/*
+	Author: Domenic Catalano
+	Student ID: 100556614
+*/
+
 #pragma once	// If you try to inlcude this more than once, it will prevent it
 
 #ifndef __CARTESIAN_POINT__
@@ -24,6 +29,10 @@ public:
 
 	double operator-(const CartesianPoint& point_to) const;
 	bool operator==(const CartesianPoint& other_point) const;
+
+	/* friend overloads */
+	friend std::ostream& operator<<(std::ostream& out, const CartesianPoint& point);
+	friend std::istream& operator>>(std::istream& in, CartesianPoint& point);
 
 	// --------------------------------------------------------------------------------
 	/* Accessors: Used to query the state of the object */
@@ -55,8 +64,8 @@ public:
 
 private:
 	// private data members for the dimensions of the point
-	int myX{}; // x-axis (horizontal) value
-	int myY{};  // y-axis (vertical) value
+	int m_x; // x-axis (horizontal) value
+	int m_y;  // y-axis (vertical) value
 };
 
 #endif /* defined (__CARTESIAN_POINT__) */
